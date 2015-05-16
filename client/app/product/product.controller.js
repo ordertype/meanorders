48fd6401b62b08973af7c5079411258b7414724a
Gustavo Dobro <gustavo.dobro@arrisi.com>
2015-05-15 23:30:50 -0300

    Adding products

--- client/app/product/product.controller.js
+++ client/app/product/product.controller.js
@@ -3,14 +3,29 @@
 angular.module('meanordersApp')
   .controller('ProductCtrl', function ($scope, $http, Auth, User) {
 
+  	$scope.awesomeProducts = [];
+
+    $http.get('/api/products').success(function(awesomeProducts) {
+      $scope.awesomeProducts = awesomeProducts;
+      socket.syncUpdates('product', $scope.awesomeProducts);
+    });
+
    
-     $scope.addProduct = function() {
+    $scope.addProduct = function() {
       if($scope.newProduct === '') {
         return;
       }
       $http.post('/api/products', { name: $scope.newProduct });
       $scope.newProduct = '';
     };
+
+    $scope.deleteProduct = function(product) {
+      $http.delete('/api/products/' + product._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('product');
+    });
 
 
   });
