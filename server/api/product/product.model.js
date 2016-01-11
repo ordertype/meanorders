d30d538667d821f083092c9eca3f581c7730dff0
Gustavo Dobro <mgdobro@gmail.com>
2016-01-11 00:50:50 -0300

    Introducing changes in table look&feel as well as adding new columns

--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -4,10 +4,20 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-  name: String,
-  info: String,
+  name:  { type: String, required: true, trim: true },
+  description:  { type: String, trim: true },
+  price:  {type: Number, get: getPrice, set: setPrice }, 
+  lastUpdate  :  { type: Date, default: Date.now },
   active: Boolean
 });
+
+function getPrice(num){
+    return (num/100).toFixed(2);
+}
+
+function setPrice(num){
+    return num*100;
+}
 
 module.exports = mongoose.model('Product', ProductSchema);
 
